59d606377033b2efbec0ed193461813e37fb4813
Claire Miao <claire.q.miao@gmail.com>
2021-08-16 18:27:19 -0700

    Javascript functionality

--- bakery.js
+++ bakery.js
@@ -1,2 +1,17 @@
-console.log('hi');
+// Show or hide cart on click 
+function showCart() {
+	if (document.getElementById('cartinfo').style.display == 'block') {
+		document.getElementById('cartinfo').style.display = 'none';
+	} else {
+		document.getElementById('cartinfo').style.display = 'block';
+	}
+}
+
+// Add to cart 
+function addToCart() {
+	document.getElementByClassName()
+}
+
+
+document.getElementById('shoppingcart').addEventListener('click', showCart);
 
